0ed883e04ff08227f85c50577b6d8f96ca14fc1d
priyanshu007grd <priyanshu007grd@gmail.com>
2022-03-17 23:33:42 +0530

    Time: 2 ms (37.38%), Space: 6.2 MB (45.05%) - LeetHub

---
+++ 856-score-of-parentheses/856-score-of-parentheses.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int scoreOfParentheses(string s) {
+        
+        stack<int> st;
+        int score = 0;
+        for(auto c:s)
+        {
+            if(c=='(')
+            {
+                st.push(score);
+                score=0;
+            }
+            else
+            {
+                score = st.top() + max(2*score,1);
+                st.pop();
+            }
+        }
+        
+        return score;
+        
+    }
+};
