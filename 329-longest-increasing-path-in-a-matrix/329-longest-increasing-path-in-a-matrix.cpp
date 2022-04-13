501a97649c06cef71aff119c2cb96ecae4365e78
priyanshu007grd <priyanshu007grd@gmail.com>
2022-04-13 21:35:45 +0530

    Time: 50 ms (79.32%), Space: 16 MB (57.97%) - LeetHub

--- 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.cpp
+++ 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.cpp
@@ -1,29 +1,35 @@
 class Solution {
 public:
-    int moves[4][2] = { {-1,0},{1,0},{0,-1},{0,1} };
+    int dir[4][2] = { {-1,0},{1,0},{0,-1},{0,1} };
     vector<vector<int> > dp; // dp[i][j] will store maximum path length starting from matrix[i][j]
     int maxPath, n, m;
 
-    int longestIncreasingPath(vector<vector<int>>& matrix) {
+    int longestIncreasingPath(vector<vector<int>>& matrix) 
+    {
         maxPath = 0, n = size(matrix), m = size(matrix[0]);
         dp.resize(n, vector<int>(m));
+        
         for(int i = 0; i < n; i++)
             for(int j = 0; j < m; j++)
-                maxPath = max(maxPath, solve(matrix, i, j));            
+                maxPath = max(maxPath, solve(matrix, i, j)); 
+        
         return maxPath;
     }
 
 
-    int solve(vector<vector<int>>& mat, int i, int j){
+    int solve(vector<vector<int>>& mat, int i, int j)
+    {
         if(dp[i][j]) return dp[i][j];
         dp[i][j] = 1;
 
-        for(int k = 0; k < 4; k++){ 
-            int new_i = i + moves[k][0], new_j = j + moves[k][1];
+        for(int k = 0; k < 4; k++)
+        { 
+            int ii = i + dir[k][0], jj = j + dir[k][1];
             
-            if(new_i < 0 || new_j < 0 || new_i >= n || new_j >= m || mat[new_i][new_j] <= mat[i][j]) continue;
-            dp[i][j] = max(dp[i][j], 1 + solve(mat, new_i, new_j));
-        }         
+            if(ii < 0 || jj < 0 || ii >= n || jj >= m || mat[ii][jj] <= mat[i][j]) continue;
+            dp[i][j] = max(dp[i][j], 1 + solve(mat, ii, jj));
+        }
+        
         return dp[i][j];
     }
 };
