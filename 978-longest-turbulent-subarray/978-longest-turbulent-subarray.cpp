2c306d7e23e0bc02faf590cdc94a52377087ef0a
priyanshu007grd <priyanshu007grd@gmail.com>
2022-04-20 23:57:10 +0530

    Time: 117 ms (32.79%), Space: 40.4 MB (60.71%) - LeetHub

---
+++ 978-longest-turbulent-subarray/978-longest-turbulent-subarray.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maxTurbulenceSize(vector<int>& A) {
+        int increase = 1; int decrease = 1; int max_len = 1;
+        for(int i=0; i < A.size()-1; i++){
+            if(A[i] < A[i+1]){
+                increase = decrease + 1;
+                decrease = 1;
+            }
+            else if(A[i] > A[i+1]){
+                decrease = increase + 1;
+                increase = 1;
+            }
+            else{
+                increase = 1;
+                decrease = 1;
+            }
+            max_len = max(max_len, max(increase, decrease));
+        }
+        return max_len;
+    }
+};
