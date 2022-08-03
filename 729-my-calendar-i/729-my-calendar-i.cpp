368487bc75e7cf1e786c03b25f1c656a1c970162
priyanshu007grd <priyanshu007grd@gmail.com>
2022-08-03 09:55:34 +0530

    Time: 182 ms (42.57%), Space: 37.8 MB (77.01%) - LeetHub

--- 729-my-calendar-i/729-my-calendar-i.cpp
+++ 729-my-calendar-i/729-my-calendar-i.cpp
@@ -1,22 +1,48 @@
 class MyCalendar {
-    map<int,int> m;
 public:
+    vector<pair<int,int>> bookingList;
     MyCalendar() {
-        m.clear();
+        
     }
     
     bool book(int start, int end) {
         
-        auto it = m.upper_bound(start);
-        
-        if(it!=m.begin() && prev(it)->second > start) return false;
-        if(it!=m.end() && it->first<end) return false;
-        
-        m[start]=end;
+        //cout<<start<<"  "<<end<<endl;
+        for(auto [s,e] : bookingList)
+        {
+            if( max(s,start)<min(e,end) )
+            {
+                //cout<<booking.first<<"  "<<booking.second<<endl;
+                //cout<<endl;
+                return false;
+            }
+                
+        }
+        bookingList.push_back({start,end});
+        //cout<<endl;
         return true;
-        
     }
 };
+
+// class MyCalendar {
+//     map<int,int> m;
+// public:
+//     MyCalendar() {
+//         m.clear();
+//     }
+    
+//     bool book(int start, int end) {
+        
+//         auto it = m.upper_bound(start);
+        
+//         if(it!=m.begin() && prev(it)->second > start) return false;
+//         if(it!=m.end() && it->first<end) return false;
+        
+//         m[start]=end;
+//         return true;
+        
+//     }
+// };
 
 /**
  * Your MyCalendar object will be instantiated and called as such:
