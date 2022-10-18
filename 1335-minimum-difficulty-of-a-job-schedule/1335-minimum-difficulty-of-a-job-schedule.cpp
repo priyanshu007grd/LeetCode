e2f4370c74cb4ccf5a0fa2b2d6afacaa0588d89a
priyanshu007grd <priyanshu007grd@gmail.com>
2022-10-19 01:54:40 +0530

    Time: 160 ms (23.98%), Space: 7.8 MB (35.48%) - LeetHub

---
+++ 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int minDifficulty(vector<int>& jobDifficulty, int D) 
+    {
+        int n=jobDifficulty.size();
+        vector<vector<int>> dp(D,vector<int>(n,INT_MAX));
+        
+        int currMax=INT_MIN;
+        for(int i=0;i<n;i++)
+        {
+            currMax=max(currMax,jobDifficulty[i]);
+            dp[0][i]=currMax;
+        }
+        
+        
+        for(int d=1;d<D;d++)
+        {
+            for(int i=1;i<n;i++)
+            {
+                currMax=INT_MIN;
+                for(int j=i;j>0;j--)
+                {
+                    currMax=max(currMax,jobDifficulty[j]);
+                    
+                    if(dp[d-1][j-1]!=INT_MAX)
+                        dp[d][i]=min(dp[d][i],currMax+dp[d-1][j-1]);
+                }
+            }
+        }
+        
+        return dp[D-1][n-1]==INT_MAX?-1:dp[D-1][n-1];
+        
+    }
+};
