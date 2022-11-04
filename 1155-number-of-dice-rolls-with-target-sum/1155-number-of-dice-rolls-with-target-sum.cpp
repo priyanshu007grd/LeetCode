bedcd55900e6c3bfb858dc704db4527dc8791221
priyanshu007grd <priyanshu007grd@gmail.com>
2022-11-05 02:56:37 +0530

    Time: 17 ms (90.67%), Space: 10.6 MB (8.07%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
@@ -1,3 +1,28 @@
+// Same question : https://www.interviewbit.com/problems/dice-throw/
+class Solution {
+public:
+    
+    int numRollsToTarget(int N, int K, int target) 
+    {
+        long long MOD = 1000000007;
+        vector<vector<long long>> dp(N+1,vector<long long>(target+1,0));
+        dp[0][0]=1;
+
+        for(int i=1;i<=N;i++) {
+            for(int j=1;j<=target;j++) {
+                dp[i][j] = (dp[i][j-1] + dp[i-1][j-1])%MOD;
+                if(j-K-1>=0) 
+                {
+                    dp[i][j] = (dp[i][j] - dp[i-1][j-K-1] + MOD )%MOD;
+                }
+            }
+        }
+
+        return dp[N][target];
+    }
+};
+
+/*
 class Solution {
 public:
     #define MOD 1000000007
@@ -19,6 +44,6 @@ public:
         }
         
         return dp[N][target];
-        
     }
 };
+*/
