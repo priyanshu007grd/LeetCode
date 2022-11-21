171919135957d60715726b19916c24605f645de9
priyanshu007grd <priyanshu007grd@gmail.com>
2022-11-22 00:07:39 +0530

    Time: 58 ms (44.60%), Space: 32.3 MB (8.58%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.cpp
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -4,19 +4,20 @@ public:
     {
         int n=nums.size();
         
-        vector<int> l(n,0);
-        vector<int> r(n,0);
+        vector<int> leftSum(n,0);
+        vector<int> rightSum(n,0);
                 
-        r[n-1]=nums[n-1];
-        for(int i=n-2;i>=0;i--) r[i]=nums[i]+r[i+1];
+        rightSum[n-1]=nums[n-1];
+        for(int i=n-2;i>=0;i--) rightSum[i]=nums[i]+rightSum[i+1];
         
-        l[0]=nums[0];
-        if(l[0]==r[0]) return 0;
+        leftSum[0]=nums[0];
+        if(leftSum[0]==rightSum[0]) return 0;
         for(int i=1;i<n;i++)
         {
-            l[i]=nums[i]+l[i-1];
-            if(l[i]==r[i]) return i;
+            leftSum[i]=nums[i]+leftSum[i-1];
+            if(leftSum[i]==rightSum[i]) return i;
         }
+        
         
         return -1;
     }
