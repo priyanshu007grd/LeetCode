63ce70e80931332b1c81efd4b7fd2da3c958b0a0
priyanshu007grd <priyanshu007grd@gmail.com>
2022-11-30 22:51:07 +0530

    Time: 637 ms (27.98%), Space: 107.8 MB (38.13%) - LeetHub

--- 0381-insert-delete-getrandom-o1-duplicates-allowed/0381-insert-delete-getrandom-o1-duplicates-allowed.cpp
+++ 0381-insert-delete-getrandom-o1-duplicates-allowed/0381-insert-delete-getrandom-o1-duplicates-allowed.cpp
@@ -1,3 +1,15 @@
+/*
+edge case where order of erase is very important
+
+    eg 1                            ["RandomizedCollection","insert","remove","insert","remove","getRandom","getRandom","getRandom","getRandom,"getRandom","getRandom","getRandom","getRandom","getRandom","getRandom"]
+[[],[0],[0],[-1],[0],[],[],[],[],[],[],[],[],[],[]]
+
+
+eg 2
+["RandomizedCollection","insert","insert","insert","insert","insert","insert","remove","remove","remove","remove","getRandom","getRandom","getRandom","getRandom","getRandom","getRandom","getRandom","getRandom","getRandom","getRandom"]
+[[],[1],[1],[2],[1],[2],[2],[1],[2],[2],[2],[],[],[],[],[],[],[],[],[],[]]
+
+*/
 class RandomizedCollection 
 {
     vector<int> v;
@@ -10,26 +22,14 @@ public:
     }
     
     bool insert(int val) 
-    {
-        // cout<<"insert "<<val<<endl;
-        // print(v);
-        // print(m);
-        
+    {        
         v.push_back(val);
         m[val].insert(v.size()-1);
-        
-        // print(v);
-        // print(m);
-        
         return m[val].size()==1;
     }
     
     bool remove(int val) 
-    {      
-        // cout<<"remove "<<val<<endl;
-        // print(v);
-        // print(m);
-        
+    {              
         if(m.find(val)==m.end()) return false;
         
         int i=*(m[val].begin());
@@ -37,18 +37,7 @@ public:
         
         v[i]=v[j];
         
-/*
-order of erase is very important
-
-    eg 1                            ["RandomizedCollection","insert","remove","insert","remove","getRandom","getRandom","getRandom","getRandom,"getRandom","getRandom","getRandom","getRandom","getRandom","getRandom"]
-[[],[0],[0],[-1],[0],[],[],[],[],[],[],[],[],[],[]]
-
-
-eg 2
-["RandomizedCollection","insert","insert","insert","insert","insert","insert","remove","remove","remove","remove","getRandom","getRandom","getRandom","getRandom","getRandom","getRandom","getRandom","getRandom","getRandom","getRandom"]
-[[],[1],[1],[2],[1],[2],[2],[1],[2],[2],[2],[],[],[],[],[],[],[],[],[],[]]
-
-*/
+        //order of erase is very important
         m[val].erase(i);
         m[v[j]].insert(i);
         m[v[j]].erase(j);
@@ -57,41 +46,15 @@ eg 2
         
         if(m[val].size()==0) m.erase(val);
         
-        // print(v);
-        // print(m);
-        
         return true;
     }
     
     int getRandom()
     {
-        // cout<<"getRandom "<<v[rand() % v.size()]<<endl;
-        // print(v);
-        // print(m);
+
         return v[rand() % v.size()];
     }
     
-    void print(vector<int> v)
-    {
-        cout<<"vector print"<<endl;
-        for(auto i:v) cout<<i<<"    ";
-        cout<<endl;
-    }
-    void print(unordered_map<int, unordered_set<int>> m)
-    {
-        cout<<"map print"<<endl;
-        for(auto i:m)
-        {
-            cout<<"val "<<i.first<<endl;
-            cout<<"pos ";
-            for(auto j:i.second)
-            {
-                cout<<j<<"  ";
-            }
-            cout<<endl;
-        }
-        cout<<endl;
-    }
 };
 
 /**
