c3f7b2bdf1474faf48c58bc3d748b19128eefb2a
priyanshu007grd <priyanshu007grd@gmail.com>
2023-08-21 22:44:40 +0530

    Time: 85 ms, Memory: 59.7 MB - LeetHub

--- peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.cpp
+++ peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.cpp
@@ -12,3 +12,16 @@ public:
         return l;
     }
 };
+
+/*
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& arr) {
+        int i = 0;
+        while (arr[i] < arr[i + 1]) {
+            i++;
+        }
+        return i;
+    }
+};
+*/
